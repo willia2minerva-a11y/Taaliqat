54138ec1e00ecff5ebeae4bf97b880d1ee148afa
William minerva <willia2minerva@gmail.com>
2026-08-07 11:31:44 +0100

    Update Post.js

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   postId: { type: String, required: true, unique: true },
+  contentSnippet: { type: String },
   processedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Post', postSchema);
-
